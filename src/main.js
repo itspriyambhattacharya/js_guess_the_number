ee5554ad2189bafeb4edc2c1a9fbfdeed102a2f6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-18 21:20:47 +0530

    feat: add, remove class dynamically using classList

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import "./style.css";
 
 const btn = document.querySelector("#submit-btn > button");
 const inp = document.querySelector(`input[type="number"`);
+const res = document.querySelector("#result-sec");
 
 console.log(btn);
 console.log(inp);
+console.log(res);
 
 // random number generation
 const rand = Math.floor(Math.random() * 100 + 1);
@@ -13,5 +15,12 @@ console.log(`The random number generated is: ${rand}`);
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   let val = inp.value;
+  res.classList.remove("hidden");
+  res.classList.add("block");
+
+  if (val === rand) {
+    let elem = document.createElement("p");
+    elem.innerHTML = "Successfull guess";
+  }
   console.log(val);
 });
