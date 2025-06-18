4dfdd33dbcb06ef61e501bdc533ba1b4b6604c4c
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-18 21:07:57 +0530

    feat: generated a random number

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import "./style.css";
 
 const btn = document.querySelector("#submit-btn > button");
 const inp = document.querySelector(`input[type="number"`);
+
 console.log(btn);
 console.log(inp);
+
+// random number generation
+const rand = Math.floor(Math.random() * 100 + 1);
+console.log(`The random number generated is: ${rand}`);
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
