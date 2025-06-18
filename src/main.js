6c08a9218bc9a35d93787bb5f380e0714c322499
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-18 21:04:12 +0530

    feat: javascript code to capture btn and input field

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const btn = document.querySelector("#submit-btn > button");
+const inp = document.querySelector(`input[type="number"`);
+console.log(btn);
+console.log(inp);
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let val = inp.value;
+  console.log(val);
+});
