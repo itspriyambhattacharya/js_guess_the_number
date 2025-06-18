3b93fb27323c37022bb29f3472c68c22642e6fd1
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-18 23:16:09 +0530

    minor update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/js_guess_the_number/",
   server: {
     host: "0.0.0.0",
   },
